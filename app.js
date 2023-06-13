25976070b96cee74e8869eea2ee2fcdf98b56d0c
Geunseok Byeon <byeongeunseok@gmail.com>
2023-06-13 21:46:22 +0900

    add fill , clear, and erase

--- app.js
+++ app.js
@@ -2,14 +2,21 @@
 
 const color = document.getElementById("color");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const modeBtn = document.getElementById("mode-btn");
+const clearBtn = document.getElementById("clear-btn");
+const eraseBtn = document.getElementById("erase-btn");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-let isPainting = false;
+const CANVAS_WIDTH = 650;
+const CANVAS_HEIGHT = 650;
 
-canvas.width = 650;
-canvas.height = 650;
+let isPainting = false;
+let isFilling = false;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 
 function onMove(event) {
@@ -46,12 +53,42 @@ function onColorClick(event) {
   color.value = colorValue;
 }
 
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onClearClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraseClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
-
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+clearBtn.addEventListener("click", onClearClick);
+eraseBtn.addEventListener("click", onEraseClick);
