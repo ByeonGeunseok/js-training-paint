41124de36e8a6f851f9b0a58af43ad0bfa6b16c0
Geunseok Byeon <byeongeunseok@gmail.com>
2023-06-13 20:48:49 +0900

    create drag action

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
+"use strict";
+
 // Set the canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
+let isPainting = false;
+
 canvas.width = 650;
 canvas.height = 650;
+ctx.lineWidth = 2;
 
-ctx.rect(50, 50, 100, 200);
-ctx.fillStyle = "purple";
-ctx.fill();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
 
-ctx.rect(250, 250, 100, 100);
-ctx.stroke();
+function onMouseDown(event) {
+  isPainting = true;
+}
 
-ctx.beginPath();
-ctx.rect(150, 250, 50, 50);
-ctx.rect(200, 200, 50, 50);
-ctx.fillStyle = "skyblue";
-ctx.fill();
+function onMouseUp(event) {
+  isPainting = false;
+}
 
-ctx.moveTo(400, 400);
-ctx.lineTo(450, 350);
-ctx.lineTo(550, 350);
-ctx.lineTo(500, 400);
-ctx.lineTo(400, 400);
-ctx.lineTo(400, 500);
-ctx.lineTo(500, 500);
-ctx.lineTo(500, 400);
-
-ctx.moveTo(550, 350);
-ctx.lineTo(550, 450);
-ctx.lineTo(500, 500);
-
-ctx.stroke();
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
